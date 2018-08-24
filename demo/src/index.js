f0e36658eb01f1fe770dae97ec4f9ff1bf91c643
Azamat <amukhamejanov@icore.kz>
2018-08-24 16:09:23 +0600

    got working redux app

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import store, { history } from './store'
+import App from './containers/app'
 
-import Table from './../../src/Table'
+import 'sanitize.css/sanitize.css'
+import './index.css'
 
-const App = () => (<Table />)
+const target = document.querySelector('#root')
 
-render(<App />, document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+)
